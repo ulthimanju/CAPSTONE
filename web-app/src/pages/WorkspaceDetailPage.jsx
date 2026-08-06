ff2280cdc0aeebc0b42556b9538721d5c85ab3c8
ULTHI MANJUNATH <umanjunath2003@gmail.com>
2026-08-06 20:14:34 +0530

    fix: remove view text icon from workspace documents list

--- web-app/src/pages/WorkspaceDetailPage.jsx
+++ web-app/src/pages/WorkspaceDetailPage.jsx
@@ -714,18 +714,6 @@ export const WorkspaceDetailPage = () => {
                           </svg>
                         </a>
                       )}
-                      {(doc.status === 'READY' || doc.status === 'READY_FOR_RAG') && (
-                        <span
-                          className="icon-btn"
-                          title="View Text"
-                          onClick={() => handleViewMarkdown(doc)}
-                        >
-                          <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                            <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
-                            <path d="M14 2v6h6" />
-                          </svg>
-                        </span>
-                      )}
                       <span
                         className="icon-btn danger"
                         title="Delete"
