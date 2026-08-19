f7d735f06f5d5e42fb9393449e8538e46a92c405
ULTHI MANJUNATH <umanjunath2003@gmail.com>
2026-08-19 21:59:21 +0530

    build(docker): copy shared module in microservice Dockerfiles and export claims helper

--- shared/shared/security/__init__.py
+++ shared/shared/security/__init__.py
@@ -1,5 +1,11 @@
 from shared.security.claims import JWTClaims
 from shared.security.jwt import JWTManager, JWTSettings
-from shared.security.auth import verify_user_identity
+from shared.security.auth import verify_user_identity, get_authenticated_claims
 
-__all__ = ["JWTClaims", "JWTManager", "JWTSettings", "verify_user_identity"]
+__all__ = [
+    "JWTClaims",
+    "JWTManager",
+    "JWTSettings",
+    "verify_user_identity",
+    "get_authenticated_claims",
+]
